54ef2e7f4e8de6efcfaef7569908d481c0781a32
jhart654 <143129762+jhart654@users.noreply.github.com>
2024-03-22 18:28:59 -0400

    Add files via upload

---
+++ JavaScript API homework/main.js
@@ -0,0 +1,30 @@
+urlFig1 = 'https://api.artic.edu/api/v1/artworks/193664'
+urlFig2 = 'https://api.artic.edu/api/v1/artworks/149776'
+urlFig3 = 'https://api.artic.edu/api/v1/artworks/145681'
+urlFig4 = 'https://api.artic.edu/api/v1/artworks/679'
+urlFig5 = 'https://api.artic.edu/api/v1/artworks/143'
+urlFig6 = 'https://api.artic.edu/api/v1/artworks/67958'
+
+function openPopup1() {
+    let popup = window.open(urlFig1, "Popup Window", "width=500, height=500");
+}
+
+function openPopup2() {
+    let popup = window.open(urlFig2, "Popup Window", "width=500, height=500");
+}
+
+function openPopup3() {
+    let popup = window.open(urlFig3, "Popup Window", "width=500, height=500");
+}
+
+function openPopup4() {
+    let popup = window.open(urlFig4, "Popup Window", "width=500, height=500");
+}
+
+function openPopup5() {
+    let popup = window.open(urlFig5, "Popup Window", "width=500, height=500");
+}
+
+function openPopup6() {
+    let popup = window.open(urlFig6, "Popup Window", "width=500, height=500");
+}
